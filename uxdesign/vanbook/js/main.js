303de01b052c9040929380407d8f58207d58b9c0
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2020-09-01 19:31:35 -0300

    terminando aula vanbook

--- uxdesign/vanbook/js/main.js
+++ uxdesign/vanbook/js/main.js
@@ -8,3 +8,27 @@ $('.modal').on('click', function(modal) {
 		$(this).removeClass('modal-ativo');
 	}
 });
+
+//Função para determinar o comprimento de cada path
+$('svg path').each(function(){
+	var comprimento = $(this).get(0).getTotalLength();
+	var comprimentoArredondado = Math.round(comprimento);
+	$(this).attr('stroke-dasharray', comprimentoArredondado);
+	$(this).attr('stroke-dashoffset', comprimentoArredondado);
+});
+
+// Função para definir a distancia da box em relação ao topo do documento HTML.
+function boxTop(idBox) {
+	var boxOffset = $(idBox).offset();
+	return boxOffset.top;
+}
+
+$(document).scroll(function() {
+	var documentTop = $(this).scrollTop();
+
+	if (documentTop > boxTop('.vantagens') - 400) {
+		$('svg').addClass('animate');
+	} else {
+		$('svg').removeClass('animate');
+	}
+});
